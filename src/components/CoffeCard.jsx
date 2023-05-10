4b3d1b39e2c722e8069a0169a78cc83829795e90
Hasib24 <hasibislam1488@gmail.com>
2023-05-10 23:58:41 +0600

    delet data and show sweet alart

--- src/components/CoffeCard.jsx
+++ src/components/CoffeCard.jsx
@@ -1,21 +1,57 @@
 import React from 'react';
+import swal from 'sweetalert';
 
 const CoffeCard = ({coffee}) => {
+    const {_id, name, chef, test, suplier } = coffee;
+
+    const handleDelete = (_id) =>{
+        
+        swal({
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+
+                fetch(`http://localhost:5000/addcoffee/${_id}`,{
+                    method: 'DELETE'
+                })
+                .then(res => res.json())
+                .then(data =>{
+                    if(data.deletedCount > 0){
+                  swal("Poof! Your imaginary file has been deleted!", {
+                    icon: "success",
+                  });
+                    }
+                })
+            
+            }
+
+
+          });
+    }
     
     return (
         <div className='border p-10'>
             <div className='flex justify-between px-5'>
-                <h1>Name : {coffee.name}</h1>
-                <h1>Chef : {coffee.chef}</h1>
+                <h1>Name : {name}</h1>
+                <h1>Chef : {chef}</h1>
             </div>
             <div className='flex justify-between px-5'>
-                <h1>Suplier : {coffee.suplier}</h1>
-                <h1>Test : {coffee.test}</h1>
+                <h1>Suplier : {suplier}</h1>
+                <h1>Test : {test}</h1>
             </div>
             <div className="btn-group btn-group-horizontal">
                 <button className="btn mx-1">Button</button>
                 <button className="btn mx-1">Edit</button>
-                <button className="btn mx-1">Delete</button>
+                <button
+                    onClick={()=>handleDelete(_id)}
+                    className="btn mx-1"
+                    
+                    >Delete</button>
             </div>
         </div>
     );
