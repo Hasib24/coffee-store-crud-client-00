bf6bf26eb4bd0f1b95e985138982a6d2ee5d13b0
Hasib24 <hasibislam1488@gmail.com>
2023-05-16 01:24:46 +0600

    conditionaly render header nav menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import Logo from '../../assets/images/more/20.png'
+import { AuthContext } from '../../provider/AuthContextProvider';
 
 const Header = () => {
+    const { user } = useContext(AuthContext);
     return (
         <div className='py-5 border flex justify-between items-center px-10'>
             <div> <img src={Logo} alt="" /></div>
             <nav className='text-2xl'>
                 <NavLink className='p-1'>Home</NavLink>
                 <NavLink className='p-1' to={'/coffees'}>Coffees</NavLink>
-                <NavLink className='p-1' to={'/addcoffee'}>Add coffee</NavLink>
-                <NavLink className='p-1' to={'/signin'}>SignIn</NavLink>
-                <NavLink className='p-1' to={'logout'}>Logout</NavLink>
+
+                {user ? <>
+                    <NavLink className='p-1' to={'/addcoffee'}>Add coffee</NavLink>
+                    <NavLink className='p-1' to={'logout'}>Logout</NavLink>
+                </>
+                : 
+                    <NavLink className='p-1' to={'/signin'}>SignIn</NavLink>
+
+                }
+
             </nav>
         </div>
     );
